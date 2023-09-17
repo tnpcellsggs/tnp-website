3ca1c62f0c1619595134764b3035333db5c3c8dc
HarromPS <anirudhsharma0192837465@gmail.com>
2023-09-17 23:16:06 +0530

    Resolved revokes

--- server/server.js
+++ server/server.js
@@ -1,44 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const dotenv = require("dotenv");
-// const { default: mongoose } = require("mongoose");
-// const adminRoute = require("./routes/adminroute");
-// const certificateRoute = require("./routes/certificateroute");
-// const eventRoute = require("./routes/eventroute");
-
-// const app = express();
-// dotenv.config();
-
-// app.use(cors());
-
-// mongoose.connect(process.env.MONGOURI, (err) => {
-//   if (err) console.log(err);
-//   else console.log("Connected to MongoDB");
-// });
-
-// // mongoose.connect(mongodb+srv://sggs-tnp:d1409XSSoVaNhLqk@sggs-tnp.n15thks.mongodb.net/test?retryWrites=true&w=majority, { user: process.env.MONGO_USER, pass: process.env.MONGO_PASSWORD, useNewUrlParser: true, useUnifiedTopology: true })
-
-// app.use(express.json());
-
-// app.get("/", (req, res) => {
-//   res.send("Welcome to homepage");
-// });
-
-// app.use("/admin/signin/", adminRoute);
-// app.use("/admin/cert/", certificateRoute);
-// app.use("/admin/events/", eventRoute);
-
-// app.get("/admin/signin", (req, res) => {
-//   res.send("Welcome to admin sign in");
-// });
-
-// const PORT = process.env.PORT || 8000;
-// app.listen(PORT, () => {
-//   console.log(`Server is running on port ${PORT}`);
-// });
-
-
-
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
@@ -79,3 +38,43 @@ const PORT = process.env.PORT || 3010; // Use 3010 as the default port
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
+// const express = require("express");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// const { default: mongoose } = require("mongoose");
+// const adminRoute = require("./routes/adminroute");
+// const certificateRoute = require("./routes/certificateroute");
+// const eventRoute = require("./routes/eventroute");
+
+// const app = express();
+// dotenv.config();
+
+// app.use(cors());
+
+// mongoose.connect(process.env.MONGOURI, (err) => {
+//   if (err) console.log(err);
+//   else console.log("Connected to MongoDB");
+// });
+
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Welcome to homepage");
+// });
+
+// app.use("/admin/signin/", adminRoute);
+// app.use("/admin/cert/", certificateRoute);
+// app.use("/admin/events/", eventRoute);
+
+// app.get("/admin/signin", (req, res) => {
+//   res.send("Welcome to admin sign in");
+// });
+
+// const PORT = process.env.PORT || 8000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+
